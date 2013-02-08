c38feee014599a37aee508f290e551ad58ff7e7b
Goutte <antoine.goutenoir@gmail.com>
2013-02-08 17:46:47 +0100

    Sort icosahedron faces so that they're ordered in an adjacency continuum

--- src/extras/geometries/IcosahedronGeometry.js
+++ src/extras/geometries/IcosahedronGeometry.js
@@ -1,25 +1,26 @@
 /**
  * @author timothypratley / https://github.com/timothypratley
+ * @author Goutte
  */
 
 THREE.IcosahedronGeometry = function ( radius, detail ) {
 
-	var t = ( 1 + Math.sqrt( 5 ) ) / 2;
+  var t = ( 1 + Math.sqrt( 5 ) ) / 2;
 
-	var vertices = [
-		[ -1,  t,  0 ], [  1, t, 0 ], [ -1, -t,  0 ], [  1, -t,  0 ],
-		[  0, -1,  t ], [  0, 1, t ], [  0, -1, -t ], [  0,  1, -t ],
-		[  t,  0, -1 ], [  t, 0, 1 ], [ -t,  0, -1 ], [ -t,  0,  1 ]
-	];
+  var vertices = [
+    [ -1,  t,  0 ], [  1, t, 0 ], [ -1, -t,  0 ], [  1, -t,  0 ],
+    [  0, -1,  t ], [  0, 1, t ], [  0, -1, -t ], [  0,  1, -t ],
+    [  t,  0, -1 ], [  t, 0, 1 ], [ -t,  0, -1 ], [ -t,  0,  1 ]
+  ];
 
-	var faces = [
-		[ 0, 11,  5 ], [ 0,  5,  1 ], [  0,  1,  7 ], [  0,  7, 10 ], [  0, 10, 11 ],
-		[ 1,  5,  9 ], [ 5, 11,  4 ], [ 11, 10,  2 ], [ 10,  7,  6 ], [  7,  1,  8 ],
-		[ 3,  9,  4 ], [ 3,  4,  2 ], [  3,  2,  6 ], [  3,  6,  8 ], [  3,  8,  9 ],
-		[ 4,  9,  5 ], [ 2,  4, 11 ], [  6,  2, 10 ], [  8,  6,  7 ], [  9,  8,  1 ]
-	];
+  var faces = [
+    [  0, 11,  5 ], [  0,  5,  1 ], [  0,  1,  7 ], [  7,  1,  8 ], [  8,  6,  7 ],
+    [ 10,  7,  6 ], [  0,  7, 10 ], [  0, 10, 11 ], [ 11, 10,  2 ], [  6,  2, 10 ],
+    [  3,  2,  6 ], [  3,  6,  8 ], [  3,  8,  9 ], [  3,  9,  4 ], [  3,  4,  2 ],
+    [  2,  4, 11 ], [  5, 11,  4 ], [  4,  9,  5 ], [  1,  5,  9 ], [  9,  8,  1 ]
+  ];
 
-	THREE.PolyhedronGeometry.call( this, vertices, faces, radius, detail );
+  THREE.PolyhedronGeometry.call( this, vertices, faces, radius, detail );
 
 };
 
